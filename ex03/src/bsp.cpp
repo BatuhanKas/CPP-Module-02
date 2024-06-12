cf4839d39da6efcd0ff3756d5961991a96371650
Batuhan Kas <bkas@k2m03s08.42kocaeli.com.tr>
2024-06-12 19:55:40 +0300

    pushke

--- ex03/src/bsp.cpp
+++ ex03/src/bsp.cpp
@@ -6,11 +6,17 @@
 /*   By: bkas <bkas@student.42kocaeli.com.tr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 15:25:55 by bkas              #+#    #+#             */
-/*   Updated: 2024/06/12 19:40:52 by bkas             ###   ########.fr       */
+/*   Updated: 2024/06/12 19:55:34 by bkas             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* **************************** [v] INCLUDES [v] **************************** */
+
 #include "../inc/Point.hpp"
+
+/* **************************** [^] INCLUDES [^] **************************** */
+
+/* ************************* [v] CROSS PRODUCT [v] ************************* */
 
 static float crossProduct(Point const &a, Point const &b, Point const &c) {
     return (b.getX() - a.getX()).toFloat() * (c.getY() - a.getY()).toFloat() -
@@ -20,11 +26,13 @@ static float crossProduct(Point const &a, Point const &b, Point const &c) {
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
     bool b1, b2, b3;
 
-    b1 = crossProduct(point, a, b) < 0.0f;
-    b2 = crossProduct(point, b, c) < 0.0f;
-    b3 = crossProduct(point, c, a) < 0.0f;
+    b1 = crossProduct(point, a, b) < 0;
+    b2 = crossProduct(point, b, c) < 0;
+    b3 = crossProduct(point, c, a) < 0;
 
     if (b1 * b2 * b3 == 0) return false;
 
     return ((b1 == b2) && (b2 == b3));
 }
+
+/* ************************* [^] CROSS PRODUCT [^] ************************* */
